4354251daf9758a2aa48292d7ba125beb2a8ab26
fixjsl <dkd462@gmail.com>
2025-05-23 13:20:34 +0900

    일단 누적으로 바꿈꿈

--- fillna.py
+++ fillna.py
@@ -16,11 +16,11 @@ df["일시"] = pd.to_datetime(df["일시"])
 df["년도"] = df["일시"].dt.year
 
 # 연도별 평균 기온
-연평균 = df.groupby("년도")["평균기온(°C)"].mean()
+연누적기온 = df.groupby("년도")["평균기온(°C)"].sum()
 연평균2 = df.groupby("년도")["최저기온(°C)"].mean()
 연평균3 = df.groupby("년도")["최고기온(°C)"].mean()
-연평균4 = df.groupby("년도")["합계 일조시간(hr)"].mean()
-연평균5 = df.groupby("년도")["일강수량(mm)"].mean()
+연누적일조량 = df.groupby("년도")["합계 일조시간(hr)"].sum()
+연누적강수량 = df.groupby("년도")["일강수량(mm)"].sum()
 
 
 # 개화일 → 연도 + 연중 몇 번째 날인지
@@ -32,26 +32,26 @@ df2["개화일숫자"] = 개화일.dt.dayofyear
 개화일숫자 = df2.set_index("년도")["개화일숫자"]
 
 # 인덱스를 연도 기준으로 맞춰야 corr 가능
-공통연도 = 연평균.index.intersection(개화일숫자.index)
-상관관계 = 연평균.loc[공통연도].corr(개화일숫자.loc[공통연도])
+공통연도 = 연누적기온.index.intersection(개화일숫자.index)
+상관관계 = 연누적기온.loc[공통연도].corr(개화일숫자.loc[공통연도])
 
 공통연도 = 연평균2.index.intersection(개화일숫자.index)
 상관관계2 = 연평균2.loc[공통연도].corr(개화일숫자.loc[공통연도])
 공통연도 = 연평균3.index.intersection(개화일숫자.index)
 상관관계3 = 연평균3.loc[공통연도].corr(개화일숫자.loc[공통연도])
-공통연도 = 연평균4.index.intersection(개화일숫자.index)
-상관관계4 = 연평균4.loc[공통연도].corr(개화일숫자.loc[공통연도])
-공통연도 = 연평균5.index.intersection(개화일숫자.index)
-상관관계5 = 연평균5.loc[공통연도].corr(개화일숫자.loc[공통연도])
+공통연도 = 연누적일조량.index.intersection(개화일숫자.index)
+상관관계4 = 연누적일조량.loc[공통연도].corr(개화일숫자.loc[공통연도])
+공통연도 = 연누적강수량.index.intersection(개화일숫자.index)
+상관관계5 = 연누적강수량.loc[공통연도].corr(개화일숫자.loc[공통연도])
 
 
-print("개화일 vs 연평균기온 상관관계:", 상관관계)
+print("개화일 vs 연누적기온 상관관계:", 상관관계)
 print("개화일 vs 연평균기온 상관관계:", 상관관계2)
 
 print("개화일 vs 연평균기온 상관관계:", 상관관계3)
 
 
-print("개화일 vs 연평균기온 상관관계:", 상관관계4)
+print("개화일 vs 연누적일조량 상관관계:", 상관관계4)
 
-print("개화일 vs 연평균기온 상관관계:", 상관관계5)
+print("개화일 vs 연누적강수량 상관관계:", 상관관계5)
 
